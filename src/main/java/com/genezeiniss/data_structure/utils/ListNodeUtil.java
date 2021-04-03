470994f51340437186f4a1b4a6e8104ced0d20d2
genezeiniss <gene.zeiniss@behalf.com>
2021-04-03 18:26:41 +0300

    challenge: split linked list in parts

--- src/main/java/com/genezeiniss/data_structure/utils/ListNodeUtil.java
+++ src/main/java/com/genezeiniss/data_structure/utils/ListNodeUtil.java
@@ -2,7 +2,20 @@ package com.genezeiniss.data_structure.utils;
 
 import com.genezeiniss.data_structure.structure.ListNode;
 
+import java.util.List;
+
 public class ListNodeUtil {
+
+    public static ListNode constructLinkedList(List<Integer> values) {
+
+        ListNode head = null;
+
+        for (int i = values.size() - 1; i >= 0; i--) {
+            head = new ListNode(values.get(i), head);
+        }
+
+        return head;
+    }
 
     public static void printList(String msg, ListNode head) {
 
